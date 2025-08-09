4f16a692b6522a5e29817f1975ad467a10165a12
Bruins288 <bruins288@gmail.com>
2025-08-09 09:52:32 +0300

    Написан unit test для FindBook при условии возвращения null

--- SystemBookLibrary/Library.cs
+++ SystemBookLibrary/Library.cs
@@ -16,6 +16,12 @@ public class Library(int countOfBooks = 10)
     {
         get => _books.Length;
     }
+    /// <summary>
+    /// Метод добавления книги в библиотеку
+    /// </summary>
+    /// <param name="book">книга</param>
+    /// <exception cref="InvalidOperationException">Генерируется если в библиотеке нет мест</exception>
+    /// <exception cref="ArgumentException">Генерируется если в библиотеке уже есть такая книга</exception>
     public void AddBook(Book book)
     {
         if (index > countOfBooks - 1)
@@ -24,11 +30,16 @@ public class Library(int countOfBooks = 10)
         }
         if (FindBook(book.Title) is not null)
         {
-            throw new ArgumentException($"Такая книга уже есть в библиотеке.");            
+            throw new ArgumentException($"Такая книга уже есть в библиотеке.");
         }
         _books[index] = book;
         index++;
     }
+    /// <summary>
+    /// Метод поиска книги по названию.
+    /// </summary>
+    /// <param name="title">Название книги</param>
+    /// <returns>ВВозвращает название книги, если книга не найдена возвращает null</returns>
     public string? FindBook(string title)
     {
         foreach (var book in _books)
---
+++ SystemBookLibraryTest/SystemBookLibraryTest.cs
@@ -0,0 +1,30 @@
+using SystemBookLibrary;
+
+namespace SystemBookLibraryTest;
+
+public class SystemBookLibraryTest
+{
+    [Fact]
+    public void GetUnavailableBookGenerateExeption()
+    {
+        //Arrange
+        Book book = new("Идиот", "Ф. М. Достоевский", false);
+        //Act 
+        var ex = Assert.Throws<InvalidOperationException>(() => book.Borrow());
+        //Assert
+        Assert.Equal("Книгу Идиот автора Ф. М. Достоевский нельзя забронировать.", ex.Message);
+    }
+    [Fact]
+    public void GetNullIfNotFoundBook()
+    {
+        //Arrage
+        Library library = new(countOfBooks: 3);
+        library.AddBook(new Book("Бесы", " Ф. М. Достоевский"));
+        library.AddBook(new Book("Война и мир", "Л. Н. Толстой"));
+        library.AddBook(new Book("Мастер и Маргарита", "М. А. Булгаков"));
+        //Act
+        string? title = library.FindBook("Лолита");
+        //Assert
+        Assert.Null(title);
+    }
+}
