72fc5620ce01fc642261ce54ae4d01136a82a143
Bruins288 <bruins288@gmail.com>
2025-08-09 09:12:29 +0300

    Создание проектов: библиотека, тест, приложение. Создание классов Book, BookTest

---
+++ SystemBookLibrary/Book.cs
@@ -0,0 +1,56 @@
+namespace SystemBookLibrary;
+/// <summary>
+/// Описывает книгу в библиотеке
+/// </summary>
+public class Book
+{
+    /// <summary>
+    /// Название книги
+    /// </summary>
+    public string? Title { get; private set; }
+    /// <summary>
+    /// Автор книги
+    /// </summary>
+    public string? Author { get; private set; }
+    /// <summary>
+    /// Показывает забронирована ли книга
+    /// </summary>
+    public bool IsAvailable { get; private set; }
+    /// <summary>
+    /// Конструктор используется для создания экземпляра книги
+    /// </summary>
+    /// <param name="title">Название книги</param>
+    /// <param name="author">Автор книги</param>
+    /// <param name="isAvailable">Доступна ли книга для бронирования</param>
+    public Book(string title, string author, bool isAvailable = true)
+    {
+        ArgumentNullException.ThrowIfNullOrWhiteSpace(title, nameof(title));
+        ArgumentNullException.ThrowIfNullOrWhiteSpace(author, nameof(author));
+
+        Title = title;
+        Author = author;
+        IsAvailable = isAvailable;
+    }
+    /// <summary>
+    /// Бронирование книги
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Исключение за попытку забронировать книгу которая занята</exception>
+    public void Borrow()
+    {
+        IsAvailable = IsAvailable ? false : throw new InvalidOperationException($"Книгу {Title} автора {Author} нельзя забронировать.");
+    }
+    /// <summary>
+    /// Возвращает книгу в библиотеку
+    /// </summary>
+    public void Return()
+    {
+        IsAvailable = true;
+    }
+    /// <summary>
+    /// Выводит статус книги
+    /// </summary>
+    public void DisplayStatus()
+    {
+        Console.WriteLine($"Книга {Title} автора {Author} забронирована? {IsAvailable}");
+    }  
+}
---
+++ SystemBookLibraryTest/BookTest.cs
@@ -0,0 +1,17 @@
+﻿using SystemBookLibrary;
+
+namespace SystemBookLibraryTest;
+
+public class SystemBookLibraryTest
+{
+    [Fact]
+    public void GetUnavailableBookGenerateExeption()
+    {
+        //Arrange
+        Book book = new("Идиот", "Ф. М. Достоевский", false);
+        //Act 
+        var ex = Assert.Throws<InvalidOperationException>(() => book.Borrow());
+        //Assert
+        Assert.Equal("Книгу Идиот автора Ф. М. Достоевский нельзя забронировать.", ex.Message);
+    }
+}
